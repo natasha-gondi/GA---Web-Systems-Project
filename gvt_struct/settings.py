b67ba6180e31ba3560d6bac63ae91c6ce747a508
TomKingori <tomkingori66@gmail.com>
2022-11-09 11:29:20 +0300

    mysql connection + national table + national render data to template

--- gvt_struct/settings.py
+++ gvt_struct/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'gvt_struct.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'members_db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
     }
 }
 
@@ -122,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
